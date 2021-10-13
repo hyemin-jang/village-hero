b5aba6722d7b0ee45c85436bebd2b48f1cdc4543
seeun214 <seeunjin22@naver.com>
2021-10-13 23:58:05 +0900

    fix detail.html

--- src/main/java/kr/pe/villagehero/controller/ErrandController.java
+++ src/main/java/kr/pe/villagehero/controller/ErrandController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import kr.pe.villagehero.dto.ErrandDTO;
 import kr.pe.villagehero.dto.MemberDTO;
@@ -22,13 +23,18 @@ public class ErrandController {
 	private ErrandService service;
 
 	@PostMapping("errand")
-	public String insertErrand(Model model, ErrandDTO newErrand) {
+	public RedirectView insertErrand(Model model, ErrandDTO newErrand) {
 		model.getAttribute("loginMember");
 		MemberDTO.Get loginMember = (Get) model.getAttribute("loginMember");
 		long id = loginMember.getMemberId();
 		
 		service.insertErrand(id, newErrand);
-		return "성공";
+		return new RedirectView("/errandBoard/detail.html");
+	}
+	
+	@GetMapping("errandDetail")
+	public ErrandDTO getNewErrand(long id) {
+		return service.getOneErrand(id);
 	}
 
 	// json객체 배열로 errand 테이블의 모든 값
