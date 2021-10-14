19201c29814b0be93600ed9ab4d5087d1cda7f9f
gaetaegoo <gaetaegoo@gmail.com>
2021-10-14 09:09:03 +0900

    Thrus-Mor-push1

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -8,48 +8,48 @@ DROP SEQUENCE apply_seq;
 
 CREATE SEQUENCE member_seq;
 CREATE TABLE member (
-       member_id		NUMBER(10)  PRIMARY KEY,
-       email			VARCHAR2(50) NOT NULL unique,
-       password     	VARCHAR2(20) NOT NULL,
-       password2    	VARCHAR2(20) NOT NULL,
-       nickname			VARCHAR2(20) NOT NULL UNIQUE,
-       age				NUMBER(10) NOT NULL,
-       gender			CHAR NOT NULL,
-       phone			VARCHAR2(20) NOT NULL UNIQUE,
-       address			VARCHAR2(100) NOT NULL,
-       specialty1		VARCHAR2(50) NOT NULL,
-       specialty2		VARCHAR2(50), 
-       specialty3		VARCHAR2(50),
-       picture			VARCHAR2(50),
-       score			NUMBER(10) DEFAULT 0,
-       member_status 	NUMBER(2) DEFAULT 0       
+       member_id      NUMBER(10)  PRIMARY KEY,
+       email         VARCHAR2(50) NOT NULL UNIQUE,
+       password        VARCHAR2(20) NOT NULL,
+       nickname         VARCHAR2(20) NOT NULL UNIQUE,
+       yyyy            NUMBER(10) NOT NULL,
+       mm            NUMBER(10) NOT NULL,
+       dd            NUMBER(10) NOT NULL,
+       gender         CHAR NOT NULL,
+       phone         VARCHAR2(20) NOT NULL UNIQUE,
+       address         VARCHAR2(100) NOT NULL,
+       specialty1      VARCHAR2(50) NOT NULL,
+       specialty2      VARCHAR2(50), 
+       specialty3      VARCHAR2(50),
+       score         NUMBER(10) DEFAULT 0,
+       member_status    NUMBER(2) DEFAULT 0       
 );
 
 CREATE SEQUENCE errand_seq;
 CREATE TABLE errand (
-	   errand_id		NUMBER(10) PRIMARY KEY,
-	   writer 			NUMBER(10) NOT NULL,
-	   pay				NUMBER(10) NOT NULL,
-	   created_at		DATE NOT NULL,
-	   title			VARCHAR2(100) NOT NULL,
-	   content 			VARCHAR2(1000) NOT NULL,	  
-	   category			VARCHAR2(50) NOT NULL,
-	   req_location		VARCHAR2(100) NOT NULL,
-	   req_date			DATE NOT NULL,
-	   errand_status	NUMBER(2) DEFAULT 0
+      errand_id      NUMBER(10) PRIMARY KEY,
+      writer          NUMBER(10) NOT NULL,      
+      created_at      DATE NOT NULL,
+      title         VARCHAR2(100) NOT NULL,
+      content          VARCHAR2(1000) NOT NULL,     
+      category         VARCHAR2(50) NOT NULL,
+      req_location      VARCHAR2(100) NOT NULL,
+      req_date         DATE NOT NULL,
+      pay            NUMBER(10) NOT NULL,
+      errand_status   NUMBER(2) DEFAULT 0,
+      completed_at      DATE
 );
 
 CREATE SEQUENCE apply_seq;
-CREATE TABLE apply (	
-	   apply_id 		NUMBER(10) PRIMARY KEY,
-	   errand			NUMBER(10) NOT NULL,	   
-	   applicant		NUMBER(10) NOT NULL,
-	   message 			VARCHAR2(500) NOT NULL,
-	   created_at		DATE NOT NULL, 
-	   match_status		NUMBER(2) DEFAULT 0	   
+CREATE TABLE apply (   
+      apply_id       NUMBER(10) PRIMARY KEY,
+      errand_id      NUMBER(10) NOT NULL,      
+      member_id      NUMBER(10) NOT NULL,
+      message          VARCHAR2(500) NOT NULL,
+      applied_at      DATE NOT NULL, 
+      match_status      NUMBER(2) DEFAULT 0      
 );
 
 ALTER TABLE errand ADD FOREIGN KEY (writer) REFERENCES MEMBER (member_id);
-ALTER TABLE apply ADD FOREIGN KEY (applicant) REFERENCES MEMBER (member_id);
-ALTER TABLE apply ADD FOREIGN KEY (errand) REFERENCES errand (errand_id);
-
+ALTER TABLE apply ADD FOREIGN KEY (member_id) REFERENCES MEMBER (member_id);
+ALTER TABLE apply ADD FOREIGN KEY (errand_id) REFERENCES errand (errand_id);
---
+++ src/main/resources/static/js/test.js
@@ -0,0 +1,210 @@
+/*변수 선언*/
+
+
+var id = document.querySelector('#id');
+
+var pw1 = document.querySelector('#pswd1');
+var pwMsg = document.querySelector('#alertTxt');
+var pwImg1 = document.querySelector('#pswd1_img1');
+
+var pw2 = document.querySelector('#pswd2');
+var pwImg2 = document.querySelector('#pswd2_img1');
+var pwMsgArea = document.querySelector('.int_pass');
+
+var nickname = document.querySelector('#nickname');
+
+var yy = document.querySelector('#yy');
+var mm = document.querySelector('#mm');
+var dd = document.querySelector('#dd');
+
+var gender = document.querySelector('#gender');
+
+var phone = document.querySelector('#phone');
+
+var address = document.querySelector('#address');
+
+var error = document.querySelectorAll('.error_next_box');
+
+
+
+/*이벤트 핸들러 연결*/
+
+
+id.addEventListener("focusout", checkId);
+pw1.addEventListener("focusout", checkPw);
+pw2.addEventListener("focusout", comparePw);
+nickname.addEventListener("focusout", checkNickname);
+yy.addEventListener("focusout", isBirthCompleted);
+mm.addEventListener("focusout", isBirthCompleted);
+dd.addEventListener("focusout", isBirthCompleted);
+gender.addEventListener("focusout", function() {
+    if(gender.value === "성별") {
+        error[5].style.display = "block";
+    } else {
+        error[5].style.display = "none";
+    }
+})
+phone.addEventListener("focusout", checkPhoneNum);
+address.addEventListener("focusout", isAddressCorrect);
+
+
+
+
+
+/*콜백 함수*/
+
+
+function checkId() {
+    var idPattern = /[a-z0-9]{2,}@[a-z0-9-]{2,}\.[a-z0-9]{2,}/;
+    if(id.value === "") {
+        error[0].innerHTML = "필수 정보입니다.";
+        error[0].style.display = "block";
+    } else if(!idPattern.test(id.value)) {
+        error[0].innerHTML = "'이메일@도메인.com' 형태로 입력하세요.";
+        error[0].style.display = "block";
+    } else {
+        error[0].innerHTML = "유효한 이메일입니다!";
+        error[0].style.color = "#08A600";
+        error[0].style.display = "block";
+    }
+}
+
+function checkPw() {
+    var pwPattern = /[a-zA-Z0-9~!@#$%^&*()_+|<>?:{}]{8,16}/;
+    if(pwPattern.test(pw1.value) && pw1.value != "") {
+    	error[1].style.display = "none";
+    	
+        pwMsg.innerHTML = "안전";
+        pwMsg.style.display = "block";
+        pwMsg.style.color = "#03c75a";
+        
+        pwImg1.src = "images/m_icon_safe.png";
+    }
+    
+    if(pw1.value === "") {
+        error[1].innerHTML = "필수 정보입니다.";
+        error[1].style.display = "block";
+    } else if(!pwPattern.test(pw1.value)) {
+        error[1].innerHTML = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
+        error[1].style.display = "block";
+        
+        pwMsg.innerHTML = "사용불가";
+        pwMsgArea.style.paddingRight = "93px";
+        pwMsg.style.display = "block";
+        
+        pwImg1.src = "images/m_icon_not_use.png";
+    } 
+}
+
+function comparePw() {
+    if(pw2.value === pw1.value && pw2.value != "") {
+        pwImg2.src = "images/m_icon_check_enable.png";
+        error[2].style.display = "none";
+    } else if(pw2.value !== pw1.value) {
+        pwImg2.src = "images/m_icon_check_disable.png";
+        error[2].innerHTML = "비밀번호가 일치하지 않습니다.";
+        error[2].style.display = "block";
+    } 
+
+    if(pw2.value === "") {
+        error[2].innerHTML = "필수 정보입니다.";
+        error[2].style.display = "block";
+    }
+}
+
+function checkNickname() {
+    var nicknamePattern = /[a-zA-Z가-힣]{3,10}/;
+    if(nickname.value === "") {
+        error[3].innerHTML = "필수 정보입니다.";
+        error[3].style.display = "block";
+    } else if(!nicknamePattern.test(nickname.value) || nickname.value.indexOf(" ") > -1) {
+        error[3].innerHTML = "3~10자 한글 or 3~20자 영문 사용(특수기호, 공백 X).";
+        error[3].style.display = "block";
+    } else {
+        error[3].style.display = "none";
+    }
+}
+
+
+function isBirthCompleted() {
+    var yearPattern = /[0-9]{4}/;
+
+    if(!yearPattern.test(yy.value)) {
+        error[4].innerHTML = "태어난 년도 4자리를 정확하게 입력하세요.";
+        error[4].style.display = "block";
+    } else {
+        isMonthSelected();
+    }
+
+
+    function isMonthSelected() {
+        if(mm.value === "월") {
+            error[4].innerHTML = "태어난 월을 선택하세요.";
+        } else {
+            isDateCompleted();
+        }
+    }
+
+    function isDateCompleted() {
+        if(dd.value === "일") {
+            error[4].innerHTML = "태어난 날짜를 선택하세요.";
+        } else {
+            isBirthRight();
+        }
+    }
+}
+
+
+
+function isBirthRight() {
+    var datePattern = /\d{1,2}/;
+    if(!datePattern.test(dd.value) || Number(dd.value)<1 || Number(dd.value)>31) {
+        error[4].innerHTML = "생년월일을 다시 확인해주세요.";
+    } else {
+        checkAge();
+    }
+}
+
+function checkAge() {
+    if(Number(yy.value) < 1920) {
+        error[4].innerHTML = "정말이세요?";
+        error[4].style.display = "block";
+    } else if(Number(yy.value) > 2022) {
+        error[4].innerHTML = "미래에서 오셨군요!?";
+        error[4].style.display = "block";
+    } else if(Number(yy.value) > 2005) {
+        error[4].innerHTML = "만 14세 미만의 어린이는 보호자의 동의가 필요합니다.";
+        error[4].style.display = "block";
+    } else {
+        error[4].style.display = "none";
+    }
+}
+
+function checkPhoneNum() {
+    var isPhoneNum = /([01]{2})([01679]{1})([0-9]{3,4})([0-9]{4})/;
+
+    if(phone.value === "") {
+        error[6].innerHTML = "필수 정보입니다.";
+        error[6].style.display = "block";
+    } else if(!isPhoneNum.test(phone.value)) {
+        error[6].innerHTML = "형식에 맞지 않는 번호입니다.";
+        error[6].style.display = "block";
+    } else {
+        error[6].style.display = "none";
+    }
+
+function isAddressCorrect() {
+    var addressPattern = /[a-z0-9]{2,}@[a-z0-9-]{2,}\.[a-z0-9]{2,}/;
+
+    if(address.value === ""){ 
+    	error[8].innerHTML = "필수 정보입니다.";
+        error[8].style.display = "block"; 
+    } else if(!addressPattern.test(address.value)) {
+    	error[8].innerHTML = "형식에 맞지 않는 주소입니다 .";
+        error[8].style.display = "block";
+    } else {
+        error[8].style.display = "none"; 
+    }
+}
+
+}
