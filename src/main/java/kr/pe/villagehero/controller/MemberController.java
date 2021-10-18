cb64942ae5bd918bd10f4519a150f87094ffd818
gaetaegoo <gaetaegoo@gmail.com>
2021-10-18 11:20:32 +0900

    211018-am11:20

--- src/main/java/kr/pe/villagehero/controller/MemberController.java
+++ src/main/java/kr/pe/villagehero/controller/MemberController.java
@@ -57,7 +57,7 @@ public class MemberController {
 			}else if (phoneCheck == false) {
 				response.setContentType("text/html; charset=UTF-8");
 				PrintWriter out = response.getWriter();
-				out.println("<script>alert('존재하는 전화번호입니다.'); history.back();</script>");
+				out.println("<script>alert('존재하는 전화번호입니다.'); history.back(); </script>");
 				out.flush();
 			}else {
 				boolean result = service.insertMember(newMember);
